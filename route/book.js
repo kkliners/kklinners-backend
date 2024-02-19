6754687988d3742a31a16365fc1b10cf70943e92
onyillto <56700691+onyillto@users.noreply.github.com>
2024-02-19 13:26:17 +0100

    Update book.js

--- route/book.js
+++ route/book.js
@@ -14,9 +14,9 @@ route.post('/:serviceId',authMiddleware, cancelService);
 // Get all services canceled by a specific user
 route.get('/:userId/cancelled-services',authMiddleware, userCancelledServices);
 // Get all pending services 
-route.get('/services/pending',authMiddleware, getAllPendingServices);
+route.get('/services/pending',isAdmin, getAllPendingServices);
 // Get all upcoming services 
-route.get('/services/upcoming',authMiddleware, getAllUpcomingServices);
+route.get('/services/upcoming',isAdmin, getAllUpcomingServices);
 // Endpoint to get all pending services for a specific user
 route.get('/user/:userId/services/pending',authMiddleware, getUserPendingServices);
 // Endpoint to get all upcoming services for a specific user
